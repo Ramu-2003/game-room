d55e7ad7a08a132aa5ddb7ce8f19b96b6b174b3b
G RAMA KRISHNA PRASAD <132869239+Ramu-2003@users.noreply.github.com>
2026-02-11 01:27:48 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Home from './pages/Home';
-import RoomSettings from './pages/RoomSettings';
-import Lobby from './pages/Lobby';
-import GameRoom from './pages/GameRoom';
+import Home from '.src/pages/Home';
+import RoomSettings from '.src/pages/RoomSettings';
+import Lobby from '.src/pages/Lobby';
+import GameRoom from '.src/pages/GameRoom';
 
 function App() {
   return (
     <Router>
       <div className="app-container">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/settings" element={<RoomSettings />} />
-          <Route path="/lobby/:roomId" element={<Lobby />} />
-          <Route path="/game/:roomId" element={<GameRoom />} />
+          <Route path="src/" element={<Home />} />
+          <Route path="src/settings" element={<RoomSettings />} />
+          <Route path="src/lobby/:roomId" element={<Lobby />} />
+          <Route path="src/game/:roomId" element={<GameRoom />} />
         </Routes>
       </div>
     </Router>
